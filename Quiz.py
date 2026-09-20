fddfa538462514a0c04f59c865315baec19a725d
Feiwan-zhe <Zhe.Liu@student.uts.edu.au>
2026-09-20 11:46:49 +1000

    Fix print statement formatting in Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,11 @@
+print("Welcome to Network Quiz")
+print("what does DNS do?")
+print("A. Translate domain names into IP addresses")
+print("B. Encrypt all network traffic")
+
+answer = input("Enter A or B: ")
+
+if answer == "A":
+ print("Correct!")
+else:
+ print("Incorrect. The answer is A.")
